fa08d865bbbe8ecee8f7c9d4de77b17da5188fdc
KrzysztofH-com\KrzysztofH <krzysztof.hyzyk@gmail.com>
2019-07-18 12:46:21 +0200

    z js

--- index.php
+++ index.php
@@ -4,6 +4,20 @@
 
 </style>
 
+<script>
+
+    function pokaz_info(){
+        document.getElementById("info").innerHTML = "<b>dane zostały wysłane do edycji</b>";
+    }
+
+    function formularz(id,name,gender){
+        var str = " <form> <input name=imie value=" + name + "> <input name=plec value=" + gender + "> <input type=submit onclick=pokaz_info();></form>";
+        document.getElementById("f"+id).innerHTML = str;
+    }
+</script>
+    
+    <div id=info></div>
+
 <?php
     class Imiona{
         public $nazwa;
@@ -23,15 +37,20 @@
             else {
                 echo "<span class=kobieta>";
             }
-            echo $this->nazwa . " " . $this->plec . "<a href=?usun=" . $this->id . "> x </a> <a href=?edytuj=" . $this->id . "> E </a><br>";
+            echo $this->nazwa . " " . $this->plec;
+            echo "<a href=?usun=" . $this->id . "> x </a>"; 
+            echo "<a onclick=formularz(" . $this->id .",'" . $this->nazwa . "','" .$this->plec ."');> E </a><br>";
             echo "</span>";
+            
             if($bold == 1) echo "</b>";
+            echo "<div id=f" . $this->id . "> </div>";
+
         }
 
         public function edytuj(){
             echo "
             <form>
-                <input name=imie value=" . $this->nazwa . "> <input name=plec value=" . $this->plec . "> <input type=submit>
+                <input name=imie value=" . $this->nazwa . "> <input name=plec value=" . $this->plec . "> <input type=submit onclick=pokaz_info();>
             </form>
             
             ";
